fba900ecdc0f6893f3f78fd4186096abf78ce8c2
DmiSidorenko <90641562+DmiSidorenko@users.noreply.github.com>
2021-12-22 21:47:42 +0300

    Update script.js

--- project/script.js
+++ project/script.js
@@ -1,49 +1,3 @@
-//функция смены позиции навбара
-function moveNavbar(){
-    let windowInnerWidth = window.innerWidth;
-    let navbar = $("#navbar");
-    if(windowInnerWidth <= 993) {
-        navbar.addClass("fixed-bottom");
-        navbar.addClass("bg-dark");
-        $(".video-bck").remove();
-    }
-    if(windowInnerWidth >= 993){
-        navbar.removeClass("fixed-bottom");
-        navbar.removeClass("bg-dark");
-    }
-}
-
-//показ дропдауна
-function showDropdown(navbarDropdown, list){
-    $(navbarDropdown).addClass("show");
-    $(navbarDropdown).prop("aria-expanded", true);
-    $(list).addClass("show");
-}
-
-//скрытие дропдауна
-function hideDropdown(navbarDropdown, list){
-    let windowInnerWidth = window.innerWidth;
-    if(windowInnerWidth >= 993) {
-        $(navbarDropdown).removeClass("show");
-        $(navbarDropdown).prop("aria-expanded", false);
-        $(list).removeClass("show");
-    }
-}
-
-$(document).ready(function (){
-    //перемещаем навбар через ивент
-    moveNavbar();
-    $(window).resize((event)=>{
-        moveNavbar();
-    });
-
-    $("#check").change(function () {
-        if ($("#check").is(":checked")) {
-            $("#submitButton").prop("disabled", false);
-        } else {
-            $("#submitButton").prop("disabled", true);
-        }
-    });
 
 $(document).on('ready', function() {
   $('.worked_with_us_slider1').slick({
@@ -107,20 +61,3 @@ $(document).on('ready', function() {
 
   });
 });
-
-$(document).ready(function() {
- $(".FAQ_accordion .FAQ_qestion:first").addClass("active");
- $(".FAQ_accordion .FAQ_answer:not(:first)").hide();
- $(".FAQ_accordion .FAQ_qestion").click(function () {
-   $(this).next(".FAQ_answer").slideToggle("fast")
-     .siblings(".FAQ_answer:visible").slideUp("normal");
-   $(this).toggleClass("active");
-   $(this).siblings(".FAQ_qestion").removeClass("active");
- });
-});
-
-    $(".navbar-toggler").click(event => {
-        showDropdown('#navbarDropdown-1','#list-1');
-        showDropdown('#navbarDropdown-2','#list-2');
-    });
-})
